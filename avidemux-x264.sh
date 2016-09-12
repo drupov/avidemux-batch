9621aa2d5f5ba0d2f6fe542a9d3804e079830851
Dimitar Rupov <dimitar@precessionmedia.com>
2016-09-12 16:28:33 +0200

    Save output to dest

--- avidemux-x264.sh
+++ avidemux-x264.sh
@@ -3,5 +3,5 @@
 IFS=$(echo -en "\n\b")
 
 for FIL in `ls *mp4 | sort` ; do
-  yes | avidemux3_cli --load "$FIL" --run settings-x264.py --save ${FIL%.*}.avi --quit
+  yes | avidemux3_cli --load "$FIL" --run settings-x264.py --save dest/${FIL%.*}.avi --quit
 done
--- avidemux-xvid.sh
+++ avidemux-xvid.sh
@@ -3,5 +3,5 @@
 IFS=$(echo -en "\n\b")
 
 for FIL in `ls *mp4 | sort` ; do
-  yes | avidemux3_cli --load "$FIL" --run settings-xvid.py --save ${FIL%.*}.avi --quit
+  yes | avidemux3_cli --load "$FIL" --run settings-xvid.py --save dest/${FIL%.*}.avi --quit
 done
